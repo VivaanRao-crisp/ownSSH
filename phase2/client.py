02a6a32238ef1de223582d571ff0f35efdaf9173
Vivaan Rao <vivaanrao.crisp@gmail.com>
2026-05-24 23:07:13 +0530

    Create client.py for phase2

---
+++ phase2/client.py
@@ -0,0 +1,17 @@
+import socket
+from shared import send_msg, recv_msg    # import from our helper file
+
+HOST = '127.0.0.1'
+PORT = 9999
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+print(f"[+] Connected to {HOST}:{PORT}")
+
+send_msg(client_socket, b"Hello! Dr. Ganguly cannot read this anymore.")
+print("[SENT]: encrypted message")
+
+reply = recv_msg(client_socket)
+print(f"[RECEIVED]: {reply.decode()}")
+
+client_socket.close()
